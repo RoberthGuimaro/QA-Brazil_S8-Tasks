79c471173ac57653427217d2eb0969ee74a71459
RoberthGuimaro <guimaroroberth@gmail.com>
2025-08-13 16:43:53 -0400

    Complete "find_element_exercise_1.py" and "find_element_exercise_2.py"

--- find_element_exercise_1.py
+++ find_element_exercise_1.py
@@ -2,18 +2,22 @@ import time
 
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
 
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 # Abra uma nova instância do Chrome WebDriver
 driver = webdriver.Chrome()
 
 # Abra a URL especificada no navegador
-driver.get(" SERVER URL ")
+driver.get("https://cnt-67b991a3-3ecb-440b-bf82-042bf9cb1878.containerhub.tripleten-services.com?lng=pt")
 
 # Pause a execução por 2 segundos para permitir que a página carregue por completo
 time.sleep(2)
 
 # Encontre o elemento de título usando seu seletor de CSS
-driver...
+element = driver.find_element(By.CSS_SELECTOR, ".logo-disclaimer")
 
+print(element)
 # Feche o navegador e encerre a sessão do WebDriver
-driver...
+driver.quit()
--- find_element_exercise_2.py
+++ find_element_exercise_2.py
@@ -2,22 +2,28 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 # Abra uma nova instância do Chrome WebDriver
 driver = webdriver.Chrome()
 
 # Abra a URL especificada no navegador
-driver.get(" SERVER URL ")
+driver.get("https://cnt-b530c3ef-700f-45d7-833c-e91397fc424e.containerhub.tripleten-services.com?lng=pt")
 
 # Pause a execução por 2 segundos para permitir que a página carregue por completo
-time.sleep(2)
+time.sleep(20)
 
 # Encontre todos os elementos na página usando um seletor de XPath
-elements = driver...
+elements = driver.find_elements(By.XPATH, "//div[@class='dst-picker-marker']")
+
+print(len(elements))
 
 # Verifique se o número de elementos encontrados é maior que 1 usando len()
-...
+assert len(elements)>0
 
 # Feche o navegador e encerre a sessão do WebDriver
-driver...
+driver.quit()
 
